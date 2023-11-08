f58995e364d17076923a31aa27d02c94bc5a36f3
Makhsodur Rahman <makhsodurrahmanhridoy@gmail.com>
2023-11-08 23:28:17 +0600

    add product config

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import './Product.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+  import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
 const Product = (props) => {
     const { img, name, seller, ratings, price, quntity } = props.product;
+
+    const handleAddToCard = props.handleAddToCard;
+    
     return (
         <div className='product'>
             <div className='product-info'>
@@ -12,7 +17,7 @@ const Product = (props) => {
                 <p>Manufacturar : {seller}</p>
                 <p>Rating : {ratings}</p>
             </div>
-            <button className='btn-cart'>Add to cart</button>
+            <button onClick={()=>handleAddToCard(props.product)} className='btn-cart'>Add to cart <FontAwesomeIcon icon={faShoppingCart} /> </button>
         </div>
     );
 };
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -4,22 +4,29 @@ import Product from '../Product/Product';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
 
     useEffect(()=>{
         fetch('products.json')
         .then(res => res.json())
         .then(data => setProducts(data));
-    },[])
+    },[]);
+
+    const handleAddToCard = (product)=>{
+        const newCart = [...cart,product];
+        setCart(newCart);
+    }
     return (
         <div>
             <div className="shop-container">
                 <div className="product-container">
                     {
-                        products.map(product => <Product product={product} key={product.id}></Product>)
+                        products.map(product => <Product product={product} key={product.id} handleAddToCard={handleAddToCard}></Product>)
                     }
                 </div>
                 <div className="card-container">
                     <h2>card container</h2>
+                    <h3 style={{color:'black'}}>Selected Items : {cart.length}</h3>
                 </div>
             </div>
         </div>
